bced4c7f150308dfa15ee1c75fa52c3fe074e19c
brunasc7 <brunasc7@gmail.com>
2024-08-21 07:19:10 -0300

    Controle de Gastos Versao Estavel

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBr7rGqDOr7g3zs_-4JSIt3CVQvkxBY3vM",
+    authDomain: "controle-de-gastos-f0c8e.firebaseapp.com",
+    projectId: "controle-de-gastos-f0c8e",
+    storageBucket: "controle-de-gastos-f0c8e.appspot.com",
+    messagingSenderId: "566891252834",
+    appId: "1:566891252834:web:c52ea3d07c06b44c6e953b"
+};
+firebase.initializeApp(firebaseConfig);
